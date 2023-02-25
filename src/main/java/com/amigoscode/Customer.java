06165cc7d49eaa3be6e3c83b2984199871514d96
ticciano <ticianodiniz@outlook.com>
2023-02-25 18:29:49 -0300

    First Commit

---
+++ src/main/java/com/amigoscode/Customer.java
@@ -0,0 +1,55 @@
+package com.amigoscode;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/resources/comandos.sql
@@ -0,0 +1,4 @@
+INSERT INTO customer (id, age, email, name) VALUES
+(nextval('customer_id_sequence'), 30, 'email1@exemplo.com', 'Nome 1'),
+(nextval('customer_id_sequence'), 25, 'email2@exemplo.com', 'Nome 2'),
+(nextval('customer_id_sequence'), 40, 'email3@exemplo.com', 'Nome 3');
